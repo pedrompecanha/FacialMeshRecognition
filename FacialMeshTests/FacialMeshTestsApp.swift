242590985d0bf3eadbbf09692e793b2cd837b575
Pedro <pedrompecanha27@gmail.com>
2024-03-21 08:04:51 -0300

    Initial Commit

---
+++ FacialMeshTests/FacialMeshTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialMeshTestsApp.swift
+//  FacialMeshTests
+//
+//  Created by Pedro Peçanha on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FacialMeshTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
